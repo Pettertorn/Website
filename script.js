cd481f266def9c7c0fbd450e13953fd6371aaa85
Pettertorn <145584146+Pettertorn@users.noreply.github.com>
2023-12-20 02:05:14 +0100

    f

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 function updateCountdown() {
     const countdownElement = document.getElementById('clock');
-    const targetDate = new Date('2023-12-26T12:00:00'); // Set target date and time
+    const videoLink = document.getElementById('video-link');
+    const targetDate = new Date('2023-12-26T12:00:00');
     const now = new Date();
     const difference = targetDate - now;
 
     if (difference > 0) {
-        // Time calculations for days, hours, minutes, and seconds
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-        // Output the result in an element with id="clock"
-        countdownElement.innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+        // ... Countdown logic ...
     } else {
-        // If the countdown is over, write some text 
-        countdownElement.innerHTML = "Time for ez 3 chest with the boys";
+        countdownElement.innerHTML = "The countdown is over!";
+        videoLink.style.display = "block"; // Show the link
     }
 }
 
-// Update the countdown every second
 setInterval(updateCountdown, 1000);
