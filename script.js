f91369a345896baf374b529294e17240910371df
Pettertorn <145584146+Pettertorn@users.noreply.github.com>
2023-12-20 01:42:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function updateCountdown() {
+    const countdownElement = document.getElementById('clock');
+    const targetDate = new Date('2023-12-26T12:00:00'); // Set target date and time
+    const now = new Date();
+    const difference = targetDate - now;
+
+    if (difference > 0) {
+        // Time calculations for days, hours, minutes, and seconds
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="clock"
+        countdownElement.innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+    } else {
+        // If the countdown is over, write some text 
+        countdownElement.innerHTML = "Time for ez 3 chest with the boys";
+    }
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
